6a831de48a9a7759001f0905b1da166e5c32c6a9
tmcfarlane <trentonmcfarlane@gmail.com>
2016-10-20 20:59:32 -0700

    init

---
+++ Models/ITodoRepository.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace Sample_Api_AspNet.Models
+{
+    public interface ITodoRepository
+    {
+        void Add(TodoItem item);
+        IEnumerable<TodoItem> GetAll();
+        TodoItem Find(string key);
+        TodoItem Remove(string key);
+        void Update(TodoItem item);
+    }
+}
---
+++ Models/TodoItem.cs
@@ -0,0 +1,9 @@
+namespace Sample_Api_AspNet.Models
+{
+    public class TodoItem
+    {
+        public string Key { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
